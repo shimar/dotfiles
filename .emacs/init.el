691194d1ae99ed65c33380d47c06e5c5455b8a18
shimar <shima01dev@gmail.com>
2016-01-29 13:23:13 +0900

    change http to https.

--- .emacs/init.el
+++ .emacs/init.el
@@ -25,8 +25,8 @@
 ;; package lists.
 ;;
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 ;;
